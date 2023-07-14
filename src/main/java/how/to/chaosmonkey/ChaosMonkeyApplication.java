9c7dfcdd58eb5b1067d8edf6c306c59f0d77d681
niceman114 <niceman114@gmail.com>
2023-07-15 06:30:03 +0900

    Init

---
+++ src/main/java/how/to/chaosmonkey/ChaosMonkeyApplication.java
@@ -0,0 +1,13 @@
+package how.to.chaosmonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaosMonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/how/to/chaosmonkey/ChaosMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package how.to.chaosmonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosMonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
